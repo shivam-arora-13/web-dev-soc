7c7f16cb6c9d6981ae852bbd8c99bc079953326f
shivam-arora-13 <shivamarora.bt19cse@pec.edu.in>
2021-08-23 16:29:27 +0530

    animation added

---
+++ index.js
@@ -0,0 +1,20 @@
+$('.apc').hover(
+    function(){ $(".default-image").removeClass("unhide");
+    $(".apc-image").addClass("unhide") },
+    function(){ $(".default-image").addClass("unhide");
+    $(".apc-image").removeClass("unhide")  }
+)
+
+$('.saasc').hover(
+    function(){ $(".default-image").removeClass("unhide");
+    $(".saasc-image").addClass("unhide") },
+    function(){ $(".default-image").addClass("unhide");
+    $(".saasc-image").removeClass("unhide")  }
+)
+
+$('.pdc').hover(
+    function(){ $(".default-image").removeClass("unhide");
+    $(".pdc-image").addClass("unhide") },
+    function(){ $(".default-image").addClass("unhide");
+    $(".pdc-image").removeClass("unhide")  }
+)
